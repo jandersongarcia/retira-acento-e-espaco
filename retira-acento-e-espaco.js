4d048da47c047d47e4f45b131b6e026429237b46
Janderson Garcoa <70637518+jandersongarcia@users.noreply.github.com>
2020-10-09 06:57:01 -0400

    Add files via upload

---
+++ retira-acento-e-espaco.js
@@ -0,0 +1,12 @@
+$(document).on('input','#ENTRADA',function(){
+    var text = $(this).val();
+    text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'A');
+    text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'E');
+    text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'I');
+    text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'O');
+    text = text.replace(new RegExp('[ÚÙÛ]', 'gi'), 'U');
+    text = text.replace(new RegExp('[Ç]', 'gi'), 'C');
+    text = text.toLowerCase();
+    text = text.replace(/[^a-z-0-9-]+/g, '-');
+    $('#SAIDA').val(text);
+});
