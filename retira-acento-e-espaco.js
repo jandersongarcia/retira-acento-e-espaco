c182f9ca440f06a73f970d000a1fc6511cb77d54
Janderson Garcia <70637518+jandersongarcia@users.noreply.github.com>
2021-12-17 10:31:35 -0400

    JAVASCRIPT
    Atualizei o script do JQUERY para o JavaScript puro.

--- retira-acento-e-espaco.js
+++ retira-acento-e-espaco.js
@@ -1,12 +1,18 @@
-$(document).on('input','#ENTRADA',function(){
-    var text = $(this).val();
-    text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'A');
-    text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'E');
-    text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'I');
-    text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'O');
-    text = text.replace(new RegExp('[ÚÙÛ]', 'gi'), 'U');
-    text = text.replace(new RegExp('[Ç]', 'gi'), 'C');
-    text = text.toLowerCase();
-    text = text.replace(/[^a-z-0-9-]+/g, '-');
-    $('#SAIDA').val(text);
-});
+<input id="texto" type="text" >
+
+<script>
+    document.getElementById("texto").onkeyup = function(e){
+        let text = document.getElementById("texto")
+        text = text.value;
+        text = text.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'A')
+        text = text.replace(new RegExp('[ÉÈÊ]', 'gi'), 'E')
+        text = text.replace(new RegExp('[ÍÌÎ]', 'gi'), 'I')
+        text = text.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'O')
+        text = text.replace(new RegExp('[ÚÙÛ]', 'gi'), 'U')
+        text = text.replace(new RegExp('[Ç]', 'gi'), 'C')
+        text = text.toLowerCase()
+        text = text.replace(/[^a-z-0-9-]+/g, '-')
+        console.log(text)
+    }
+    
+</script>
